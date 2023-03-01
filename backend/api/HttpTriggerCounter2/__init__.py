c4566d0634b477dcc98178ac49c5f9fcc90a992a
SameeraSuhail1 <sameera.suhail@ymail.com>
2023-03-01 17:13:37 +1000

    add comment to test workflow

--- backend/api/HttpTriggerCounter2/__init__.py
+++ backend/api/HttpTriggerCounter2/__init__.py
@@ -29,6 +29,7 @@ import azure.functions as func
 import json
 from azure.cosmos import CosmosClient
 import os
+#test comment
 
 def main(req: func.HttpRequest, azureresume: func.DocumentList) -> str:
     if not azureresume:
